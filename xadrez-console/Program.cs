bba45fa92334b6802c6b94f39dba75600ab95292
laborba <lucasadeborba@gmail.com>
2023-11-28 18:15:25 -0300

    Controlando as pecas capturadas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -12,10 +12,7 @@ namespace xadrez_console {
                 while (!partida.terminada) {
                     try {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Tela.imprimirPartida (partida);
 
                         Console.WriteLine();
                         Console.Write("Origem: ");
@@ -30,7 +27,7 @@ namespace xadrez_console {
                         Console.WriteLine();
                         Console.Write("Destino: ");
                         Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
-                        partida.validarPosicaoDeDestino(origem, destino); 
+                        partida.validarPosicaoDeDestino(origem, destino);
 
                         partida.realizaJogada(origem, destino);
                     }
